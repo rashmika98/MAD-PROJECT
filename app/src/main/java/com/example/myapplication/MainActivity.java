8811dc6f147f74506269e3540e519cb88962afb4
rashmika98 <rsamuditha0@gmail.com>
2019-08-02 12:02:35 +0530

    fhs

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startNewActivity(View view)
+    {
+        Intent i = new Intent (MainActivity.this,Signup_From.class);
+        startActivity(i);
+    }
+}
