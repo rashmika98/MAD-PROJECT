99153ebca54238483c9d26753567a8cf9e4e4ce4
rashmika98 <rsamuditha0@gmail.com>
2019-09-07 15:45:25 +0530

    DriverInfo insert

--- app/src/main/java/com/example/myapplication/AddNewDeliveryBoy.java
+++ app/src/main/java/com/example/myapplication/AddNewDeliveryBoy.java
@@ -1,24 +1,83 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
+
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.Toast;
+
+        import com.google.firebase.database.DatabaseReference;
+        import com.google.firebase.database.FirebaseDatabase;
 
 public class AddNewDeliveryBoy extends AppCompatActivity {
+
+    EditText txtName, txtNIC, txtAddress, txtEmail,txtDLicense, txtuName, txtPassword;
+    Button btnadd;
+
+    DatabaseReference ref;
+
+    Driver driver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_delivery_boy);
+
+        txtName = findViewById(R.id.adtxt1);
+        txtNIC = findViewById(R.id.adtxt2);
+        txtAddress = findViewById(R.id.adtxt3);
+        txtEmail = findViewById(R.id.adtxt4);
+        txtDLicense = findViewById(R.id.adtxt5);
+        txtuName = findViewById(R.id.adtxt6);
+        txtPassword = findViewById(R.id.adtxt7);
+
+        btnadd = findViewById(R.id.adbtn1);
+
+        driver = new Driver();
+
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                driver.setName(txtName.getText().toString());
+                driver.setNic(txtNIC.getText().toString());
+                driver.setAddress(txtAddress.getText().toString());
+                driver.setEmail(txtEmail.getText().toString());
+                driver.setdLicense(Integer.valueOf(txtDLicense.getText().toString()));
+                driver.setUname(txtuName.getText().toString());
+                driver.setDpaw(txtPassword.getText().toString());
+
+                ref = FirebaseDatabase.getInstance().getReference().child("  Driver");
+                //ref.child(String.valueOf(driver.getdLicense())).setValue(driver);
+//                ref.child((driver.getName())).setValue(driver);
+
+                ref.push().setValue( driver);
+
+                //Toast.makeText(AddNewDeliveryBoy.this, "Data Inserted Successfully", Toast.LENGTH_SHORT).show();
+
+                Toast.makeText(AddNewDeliveryBoy.this, "Success", Toast.LENGTH_SHORT).show();
+
+                clearAll();
+
+            }
+        });
+
     }
 
-    public void viewDetails(View view){
-        Intent intent = new Intent(AddNewDeliveryBoy.this, ManagerEmployee.class);
-        startActivity(intent);
+
+
+
+    public void clearAll(){
+        txtName.setText("");
+        txtNIC.setText("");
+        txtAddress.setText("");
+        txtEmail.setText("");
+        txtDLicense.setText("");
+        txtuName.setText("");
+        txtPassword.setText("");
     }
-
-
 
 }
---
+++ app/src/main/java/com/example/myapplication/Driver.java
@@ -0,0 +1,80 @@
+package com.example.myapplication;
+
+public class Driver {
+
+        private  String name;
+        private  String nic;
+        private  String address;
+        private  String email;
+       // private  int number;
+        private  Integer dLicense;
+        private  String uname;
+        private  String dpaw;
+
+    public Driver() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+//    public int getNumber() {
+//        return number;
+//    }
+//
+//    public void setNumber(int number) {
+//        this.number = number;
+//    }
+
+    public Integer getdLicense() {
+        return dLicense;
+    }
+
+    public void setdLicense(Integer dLicense) {
+        this.dLicense = dLicense;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getDpaw() {
+        return dpaw;
+    }
+
+    public void setDpaw(String dpaw) {
+        this.dpaw = dpaw;
+    }
+}
