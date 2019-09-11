3247b16e580d5b71378d38d40a1c4ea203831a12
salukbawa <salukbawantha96@gmail.com>
2019-09-11 17:56:11 +0530

    Delivery report insert & delete1

--- app/src/main/java/com/example/myapplication/UserProfile.java
+++ app/src/main/java/com/example/myapplication/UserProfile.java
@@ -3,18 +3,43 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
 
 import java.util.Locale;
 
 public class UserProfile extends AppCompatActivity {
 
+
+
+
+        TextView dsplyfname,dsplyuname,dsplymail,dsplyadd,dsplypno;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_profile);
+
+        //database = new cus_details_add(this);
+
+        //dsplyfname = findViewById(R.id.user_profile_dsplyfname);
+        //dsplyuname = findViewById(R.id.user_profile_dsplyuname);
+        //dsplymail = findViewById(R.id.user_profile_dsplymail);
+        //dsplyadd = findViewById(R.id.user_profile_dsplyadd);
+        //dsplypno = findViewById(R.id.user_profile_dsplypno);
+
+
+
+    }
+
+    protected void onstart(){
+        super.onStart();
+
+        //dsplyfname.setText(data.getfname());
+        //dsplyuname.setText();
     }
     public void home(View view){
 
---
+++ app/src/main/java/com/example/myapplication/cus_details_add.java
@@ -0,0 +1,100 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class cus_details_add {
+
+    private String fname;
+    private String usrname;
+    private String email;
+    private String address;
+    private String pno;
+    private String pwrd;
+    private String conpwrd;
+   // private String male;
+    //private String female;
+
+
+    public cus_details_add() {
+    }
+
+    public String getfname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getUsrname() {
+        return usrname;
+    }
+
+    public void setUsrname(String usrname) {
+        this.usrname = usrname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPno() {
+        return pno;
+    }
+
+    public void setPno(String pno) {
+        this.pno = pno;
+    }
+
+    public String getPwrd() {
+        return pwrd;
+    }
+
+    public void setPwrd(String pwrd) {
+        this.pwrd = pwrd;}
+
+        public String getconpwrd() {
+            return conpwrd;
+        }
+
+        public void setconpwrd(String conpwrd) {
+            this.conpwrd = conpwrd;
+        }
+       //     public String getmale() {
+         //       return male;
+           // }
+
+            //public void setMale(String male) {
+              //  this.male = male;
+            //}
+    //public String getFemale() {
+      //  return female;
+    //}
+
+    //public void setFemale(String female) {
+      //  this.female = female;
+    //}
+        }
+
+
+
