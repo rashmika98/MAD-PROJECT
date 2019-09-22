6b44b5c68290c8ae33e0841e3c6848c7c22eb768
ShanAbeygunawardane <shanperera963@gmail.com>
2019-09-22 16:14:09 +0530

    add5

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -21,6 +21,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    String un, pwd;
     EditText name;
     EditText pass;
 
@@ -47,51 +48,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        layout =  (RelativeLayout) findViewById(R.id.layout1);
+        layout = (RelativeLayout) findViewById(R.id.layout1);
 
         handler.postDelayed(runnable, 1500);
 
 
-
-        name=findViewById(R.id.editText);
-        pass=findViewById(R.id.editText3);
-        login=findViewById(R.id.button);
+        name = findViewById(R.id.editText);
+        pass = findViewById(R.id.editText3);
+        login = findViewById(R.id.button);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                validate(name.getText().toString(),pass.getText().toString());
+                validate(name.getText().toString(), pass.getText().toString());
             }
         });
 
     }
 
-    public void validate(String username, String password){
+    public void validate(String username, String password) {
 
-        if((username.equals("admin")) && (password.equals("1234"))){
-            Intent intent = new Intent(MainActivity.this,ManagerDashboard.class);
+        DatabaseReference readref = FirebaseDatabase.getInstance().getReference().child("cus_details_add ").child(name.getText().toString());
+        readref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChildren()) {
+
+                    un = dataSnapshot.child("usrname").getValue().toString();
+                    pwd = dataSnapshot.child("pwrd").getValue().toString();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        if ((username.equals("admin")) && (password.equals("1234"))) {
+            Intent intent = new Intent(MainActivity.this, ManagerDashboard.class);
             startActivity(intent);
-            Toast.makeText(getApplicationContext(),"Login Pass!!", Toast.LENGTH_LONG).show();
-        }else if((username.equals("manager")) && (password.equals("5678"))){
-            Intent intent2 = new Intent(MainActivity.this,dashboard.class);
+            Toast.makeText(getApplicationContext(), "Login Pass!!", Toast.LENGTH_LONG).show();
+        } else if ((username.equals("manager")) && (password.equals("5678"))) {
+            Intent intent2 = new Intent(MainActivity.this, dashboard.class);
             startActivity(intent2);
-            Toast.makeText(getApplicationContext(),"Login Pass!!",Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Login Pass!!", Toast.LENGTH_LONG).show();
         }
 //        else if((username.equals("saluk")) && (password.equals("9900"))){
 //        Intent intent3 = new Intent(MainActivity.this,DeliveryProfile.class);
 //        startActivity(intent3);
 //        Toast.makeText(getApplicationContext(),"Login Pass!!",Toast.LENGTH_LONG).show();
 //        }
-        else if((username.equals("customer")) && (password.equals("0000"))){
-        Intent intent4 = new Intent(MainActivity.this,UserProfile.class);
-        startActivity(intent4);
-        Toast.makeText(getApplicationContext(),"Login Pass!!",Toast.LENGTH_LONG).show();
+        else {
+            if ((name.equals(un)) && (pass.equals(pwd))) {
+                Intent intent4 = new Intent(MainActivity.this, UserProfile.class);
+                startActivity(intent4);
+                Toast.makeText(getApplicationContext(), "Login Pass!!", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Invalid Password or Username", Toast.LENGTH_LONG).show();
+            }
         }
-//        else{
-//            Toast.makeText(getApplicationContext(),"Invalid Password or Username",Toast.LENGTH_LONG).show();
-//        }
+    }
 
-        else{
-
+       /* else{
+            Toast.makeText(getApplicationContext(),"Invalid Password or Username",Toast.LENGTH_LONG).show();
             DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("UserLogin").child(name.getText().toString());
 
             databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -128,12 +146,11 @@ public class MainActivity extends AppCompatActivity {
                 public void onCancelled(@NonNull DatabaseError databaseError) {
 
                 }
-            });
+            });*/
 
 
-        }
 
-    }
+
 
 
     public void startNewActivity(View view)
